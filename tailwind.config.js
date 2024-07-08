1a23a98a73b207209ecad3e220196b48b1253adc
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-07-09 03:51:34 +0600

    Added banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        source: '"Source Sans 3", "sans-serif"',
+      },
+      maxWidth: {
+        'custom': '1140px',
+      },
+    },
   },
   plugins: [],
 }
